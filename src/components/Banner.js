0af68456c6cd11fd1e4552acf995d9241b563127
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-10 12:26:56 +0200

    logo added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,13 @@
 import '../styles/Banner.css'
+import logo from '../assets/logo.png'
 
 const title = "La maison Jungle"
 
 function Banner (){
   return (
   <div className='lmj-banner'>
-      <h1>{ title.toUpperCase()}</h1>
+    <img src={logo} alt='La maison jungle' className='lmj-logo' />
+      <h1 className='lmj-title'>{ title.toUpperCase()}</h1>
   </div>
 )}
 
