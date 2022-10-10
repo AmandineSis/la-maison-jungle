2e3c807046c805d02f758b6fd5cf907adbc3a5f2
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-10 11:59:14 +0200

    Cart added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import '../App.css';
 import Banner from './Banner.js'
+import Cart from './Cart.js'
 
 function App() {
-  return <Banner />
+  return (<React.Fragment><Banner /><Cart /></React.Fragment>)
 }
 
 export default App;
---
+++ src/components/Cart.js
@@ -0,0 +1,17 @@
+const monsteraPrix = 8;
+const lierrePrix = 10;
+const fleurPrix  = 15;
+
+function Cart(){
+  return(<div>  
+    <h2>Panier</h2>
+      <ul>
+      <li>Monstera: {monsteraPrix}</li>
+      <li>Lierre: {lierrePrix}</li>
+      <li>Bouquet de fleur: {fleurPrix}</li>
+  </ul>
+      <p>Total : <span>{monsteraPrix + lierrePrix + fleurPrix}</span></p>
+  </div>)
+}
+
+export default Cart
