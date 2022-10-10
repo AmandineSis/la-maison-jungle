8aa33c5722f40bd99b5f4ad6a5c975843f253f0b
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-10 14:39:14 +0200

    ShoppingList component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Banner from './Banner.js'
 import Cart from './Cart.js'
+import ShoppingList from './ShoppingList.js'
 
 function App() {
-  return (<React.Fragment><Banner /><Cart /></React.Fragment>)
+  return (<React.Fragment>
+    <Banner />
+    <ShoppingList />
+    <Cart />
+    </React.Fragment>)
 }
 
 export default App;
