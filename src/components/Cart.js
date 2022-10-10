0b0f91c465d26e86ea7205799f5d324a3f7547af
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-10 12:30:09 +0200

    style added to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,11 @@
+import '../styles/Cart.css'
+
 const monsteraPrix = 8;
 const lierrePrix = 10;
 const fleurPrix  = 15;
 
 function Cart(){
-  return(<div>  
+  return(<div className="lmj-cart">  
     <h2>Panier</h2>
       <ul>
       <li>Monstera: {monsteraPrix}</li>
