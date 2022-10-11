4bed1a761229b3b427bee6b94add4f89531837a2
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-11 11:47:33 +0200

    water and sun icon added

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,7 +1,10 @@
+import sun from '../assets/sun.svg'
+import water from '../assets/sun.svg'
+
 function CareScale({ scaleValue, careType }) {
     const range = [1, 2, 3]
     
-    const scaleType = careType === 'light' ? '☀️' : '💧'
+    const scaleType = careType === 'light' ? <img src={sun} alt='soleil' className='lmj-logo' />: <img src={water} alt="goutte d'eau" className='lmj-logo' />
 
     return (
         <div>
