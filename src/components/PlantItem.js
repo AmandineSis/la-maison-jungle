a83bdc1bf470773ddbd0be3a08aa0ead458b2df4
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-11 11:48:10 +0200

    lmj-plantList moved from ShoppingList to PlantItem

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,3 +1,22 @@
-function PlantItem (props){
+import CareScale from "./CareScale";
+
+function PlantItem ({name, cover, id}){
+
+   return <div className="lmj-plant-item-cove">
+    <ul className='lmj-plant-list'>
+            {plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+                        {plant.name}{plant.isBestSale && <span>🔥</span>}
+                        {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+                       
+                    </li>
+				))}
+
+			</ul>
+    <CareScale careType='water' scaleValue={plant.water} />
+    <CareScale careType='light' scaleValue={plant.light} />
+   </div>
     
 }
+
+export default PlantItem
