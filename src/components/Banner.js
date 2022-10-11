23a5937d6a1c5b9c7312b8651ed55886fbf1c898
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-11 10:21:09 +0200

    recommendation.js added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,18 @@
 import '../styles/Banner.css'
 import logo from '../assets/logo.png'
-
+import Recommendation from './Recommendation'   
 const title = "La maison Jungle"
 
 function Banner (){
   return (
   <div className='lmj-banner'>
-    <img src={logo} alt='La maison jungle' className='lmj-logo' />
-      <h1 className='lmj-title'>{ title.toUpperCase()}</h1>
+    <div className='lmj-header'>
+        <img src={logo} alt='La maison jungle' className='lmj-logo' />
+        <h1 className='lmj-title'>{ title.toUpperCase()}</h1>
+    </div>
+    <Recommendation />
   </div>
+
 )}
 
 
