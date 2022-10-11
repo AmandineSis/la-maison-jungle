a1812f1b5b66b67933e6cfe01ca9a7d771f419b7
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-11 15:47:55 +0200

    style added to care and plantitem

--- src/components/Recommendation.js
+++ src/components/Recommendation.js
@@ -1,7 +1,6 @@
 function Recommendation(){
     const currentMonth = new Date().getMonth()
     const isSpring = currentMonth >= 2 && currentMonth <=4
-    console.log(currentMonth)
     if(!isSpring){
         return <div>Ce n'est pas le moment de rempoter !</div>
     } else {
