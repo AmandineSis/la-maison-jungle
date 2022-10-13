0969d2e85179085624eda839d475fd4913c3e4eb
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-13 10:27:29 +0200

    state added to cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,22 @@
 import '../styles/Cart.css'
+import  { useState } from 'react'
 
-const monsteraPrix = 8;
-const lierrePrix = 10;
-const fleurPrix  = 15;
+function Cart() {
+  const monsteraPrice = 8
+  const [cart, updateCart] = useState(0)
 
-function Cart(){
-  return(<div className="lmj-cart">  
-    <h2>Panier</h2>
-      <ul>
-      <li>Monstera: {monsteraPrix}</li>
-      <li>Lierre: {lierrePrix}</li>
-      <li>Bouquet de fleur: {fleurPrix}</li>
-  </ul>
-      <p>Total : <span>{monsteraPrix + lierrePrix + fleurPrix}</span></p>
-  </div>)
+  return (
+      <div className='lmj-cart'>
+          <h2>Panier</h2>
+          <div>
+              Monstera : {monsteraPrice}€
+              <button onClick={() => updateCart(cart + 1)}>
+                  Ajouter
+              </button>
+          </div>
+          <h3>Total : {monsteraPrice * cart}€</h3>
+      </div>
+  )
 }
 
 export default Cart
