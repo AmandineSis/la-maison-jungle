86795d1c6b50bd4fe0535aded945a514ac6ad8e5
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-13 12:06:14 +0200

    categories added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import '../styles/Cart.css'
 
-function Cart({cart, updateCart}) {
-	const monsteraPrice = 8
+function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
-
-  //If cart is open => display cart
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price,
+		0
+	)
 	return isOpen ? (
 		<div className='lmj-cart'>
 			<button
@@ -14,12 +15,24 @@ function Cart({cart, updateCart}) {
 			>
 				Fermer
 			</button>
-			<h2>Panier</h2>
-			<h3>Total : {monsteraPrice * cart}€</h3>
-      <button onClick={() => updateCart(0)}>Vider le panier</button>
+			{cart.length > 0 ? (
+				<div>
+					<h2>Panier</h2>
+					<ul>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+							</div>
+						))}
+					</ul>
+					<h3>Total :{total}€</h3>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
+				</div>
+			) : (
+				<div>Votre panier est vide</div>
+			)}
 		</div>
 	) : (
-    //else => close cart
 		<div className='lmj-cart-closed'>
 			<button
 				className='lmj-cart-toggle-button'
