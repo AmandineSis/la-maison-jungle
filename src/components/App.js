a7a9284d441c8b59effb344a7f390eae83178344
Amandine Sismondi <am.sismondi@gmail.com>
2022-10-13 15:15:06 +0200

    useEffect and localStorage added to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,24 @@ import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-	
-	const [cart, updateCart] = useState([])
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
 	return (
 		<div>
 			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
 			<div className='lmj-layout-inner'>
-				<Cart cart={cart} updateCart={updateCart}/>
-				<ShoppingList cart={cart} updateCart={updateCart}/>
+				<Cart cart={cart} updateCart={updateCart} />
+				<ShoppingList cart={cart} updateCart={updateCart} />
 			</div>
 			<Footer />
 		</div>
@@ -25,3 +29,4 @@ function App() {
 }
 
 export default App
+
